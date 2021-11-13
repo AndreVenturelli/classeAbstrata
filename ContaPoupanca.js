75bdbea70055c1ed6e0736bcc4414be7dfa6f453
AndreVenturelli <andreventurelli10@gmail.com>
2021-11-13 11:45:58 -0300

    Modificação na ContaPoupança e no index

--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -3,12 +3,11 @@ import { Conta } from "./Conta.js";
 export class ContaPoupanca extends Conta{
     constructor(saldoInicial, cliente, agencia) {
         super(saldoInicial, cliente, agencia);
+    }
 
     sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
+        const taxa = 1.02;
+        return this.sacar(valor,taxa)
     }
 
 }
